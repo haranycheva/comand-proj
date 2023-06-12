383dd44ede1b268f8285a89c1bbf1e052d274212
haranycheva <120900899+haranycheva@users.noreply.github.com>
2023-06-12 18:40:40 +0000

    Deploying to gh-pages from @ haranycheva/comand-proj@ad37cd09378a9130746a884bcc173e99b9a2274d 🚀

--- cards-page.a84cdb40.js
+++ cards-page.a84cdb40.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.eb232b92.js.map
+//# sourceMappingURL=cards-page.a84cdb40.js.map
